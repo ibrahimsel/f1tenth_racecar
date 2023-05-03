b66ad06ed98dc0fd7cca5cb26ea523d56bc1dd3b
Ibrahim Sel <ibrahim.sel58@hotmail.com>
2023-05-03 17:12:21 +0300

    parameters and reactive speed control added

--- src/pure_pursuit/launch/pure_pursuit.launch.py
+++ src/pure_pursuit/launch/pure_pursuit.launch.py
@@ -1,28 +1,29 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import os
-
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import FrontendLaunchDescriptionSource
+
 
 def generate_launch_description():
-    wp_file = "/home/labs/rcws/logs/waypoints_file.csv"
-
-
+    config_file = os.path.join(get_package_share_directory(
+        'pure_pursuit'), 'config', 'pure_pursuit.yaml')
     node_pure_pursuit = Node(
         package="pure_pursuit",
         executable="pure_pursuit",
         name="pure_pursuit",
         output="screen",
-        parameters=[
-            {"L": 1.2},
-            {"speed": 1.0},
-            {"wp_file": wp_file},
-        ]
+        parameters=[config_file]
+    )
+    node_safety = Node(
+        package="pure_pursuit",
+        executable="safety_node",
+        name="safety_node",
+        output="screen",
+        parameters=[config_file]
     )
 
     # Create LaunchDescription object and add nodes to launch
     ld = LaunchDescription()
     ld.add_action(node_pure_pursuit)
+    ld.add_action(node_safety)
     return ld
--- src/pure_pursuit/pure_pursuit/pure_pursuit.py
+++ src/pure_pursuit/pure_pursuit/pure_pursuit.py
@@ -1,45 +1,57 @@
 #!/usr/bin/python3
 from ackermann_msgs.msg import AckermannDriveStamped
-from visualization_msgs.msg import Marker, MarkerArray
-from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PoseWithCovarianceStamped
-
-import sys
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Bool
+from rclpy.node import Node
 import rclpy
 import numpy as np
-import os
 import math
-from rclpy.node import Node
-
-LOOKAHEAD_DISTANCE = 1.2
-waypoints_file = open(
-    ('/home/labs/rcws/logs/waypoints_file.csv'), 'r')
-waypoints = []
-contents = waypoints_file.read()
-lines = contents.splitlines()
-for i in lines:  # iterate over every logged waypoint
-    splitted = i.split(",")
-    x = float(splitted[0])
-    y = float(splitted[1])
-    waypoints.append([y, x])
-waypoints_file.close()
 
 
 class PurePursuit(Node):
     def __init__(self):
         super().__init__('pure_pursuit')
-        global waypoints
-        self.waypoints = waypoints
+        self.declare_parameter('L', 1.0)
+        self.declare_parameter('min_velocity', 1.0)
+        self.declare_parameter('max_velocity', 1.6)
+        self.declare_parameter(
+            'waypoints_file', '/home/labs/rcws/logs/waypoints_file.csv')
+        self.waypoints_file = self.get_parameter(
+            'waypoints_file').get_parameter_value().string_value
+        self.L = self.get_parameter('L').get_parameter_value().double_value
+        self.min_velocity = self.get_parameter('min_velocity').get_parameter_value().double_value
+        self.max_velocity = self.get_parameter('max_velocity').get_parameter_value().double_value
+
         self.pose_sub = self.create_subscription(
             PoseWithCovarianceStamped, '/amcl_pose', self.pose_callback, 10)
-        self.steering_pub = self.create_publisher(
+        self.drive_pub = self.create_publisher(
             AckermannDriveStamped, '/drive', 10)
+        self.brake_sub = self.create_subscription(Bool, '/emergency_braking', self.brake_callback, 100)
         self.visualize_pub_marker = self.create_publisher(
             Marker, '/dynamic_env', 10)
+        self.emergency_braking = False
+        self.rate = self.create_rate(5)
+        self.velocity = 0
+
+        with open((self.waypoints_file), 'r') as wp_file:
+            self.waypoints = []
+            contents = wp_file.read()
+            lines = contents.splitlines()
+            for i in lines:  # iterate over every logged waypoint
+                splitted = i.split(",")
+                x = float(splitted[0])
+                y = float(splitted[1])
+                self.waypoints.append([y, x])
+
+    def brake_callback(self, data):
+        self.emergency_braking = data.data
+
 
     def distance_to_start(self, car_point):
         return math.dist(car_point, self.waypoints[0])
-
+    
     def get_goal_waypoint(self, car_point):
         goal_waypoint = self.waypoints[1]
         distance_to_start = self.distance_to_start(car_point)
@@ -47,10 +59,7 @@ class PurePursuit(Node):
             return self.waypoints[2]
         for i in self.waypoints:
             distance = math.dist(car_point, i)
-            # below, we choose the furthest waypoint inside our lookahead distance
-            # it chooses the furthest because it chooses the last waypoint inside our lookahead distance
-            if (distance <= LOOKAHEAD_DISTANCE):
-                # prev_waypoint = goal_waypoint
+            if (distance <= self.L):
                 goal_waypoint = i
 
         marker = Marker()
@@ -75,7 +84,10 @@ class PurePursuit(Node):
         self.visualize_pub_marker.publish(marker)
         return goal_waypoint
 
-    def pose_callback(self, pose_msg):
+    def odom_callback(self, odom_msg):
+        self.speed_x = odom_msg.twist.twist.linear.x
+
+    def pose_callback(self, pose_msg):     
         car_x = pose_msg.pose.pose.position.x
         car_y = pose_msg.pose.pose.position.y
         goal_waypoint = self.get_goal_waypoint([car_y, car_x])
@@ -90,39 +102,49 @@ class PurePursuit(Node):
         cosy_cosp = 1.0 - 2.0 * \
             (quaternion[1] * quaternion[1] + quaternion[2] * quaternion[2])
         heading_current = np.arctan2(siny_cosp, cosy_cosp)
-        # print(f"siny_cosp: {siny_cosp} / cosy_cosp: {cosy_cosp} / heading_current: {heading_current}")
         euclidean_dist = math.dist(goal_waypoint, [car_y, car_x])
         lookahead_angle = np.arctan2(
             goal_waypoint[0] - car_y, goal_waypoint[1] - car_x)
         delta_y = euclidean_dist * np.sin(lookahead_angle - heading_current)
-        angle = self.calculate_steering_angle(
-            LOOKAHEAD_DISTANCE, delta_y)
-        velocity = self.calculate_velocity(angle)
-        
-        self.publish_steering(velocity, angle)
+
+        steering_angle = self.calculate_steering_angle(
+            self.L, delta_y)
+        self.velocity = self.calculate_velocity(lookahead_angle)
+        self.get_logger().warn(f'velocity is: {self.velocity:.2f} | curv_angle is {lookahead_angle:.2f}')
+        self.publish_steering(self.velocity, steering_angle)
+
 
     # calculates the curvature of the arc
     def calculate_steering_angle(self, L, y):
         return (2*y) / (math.pow(L, 2))
 
-    def calculate_velocity(self, angle):
-        angle = abs(math.degrees(angle))
+    def calculate_velocity(self, lookahead_angle):
+        angle_min = -math.pi
+        angle_max = math.pi
+        self.velocity = self.value_mapper(lookahead_angle, angle_min, angle_max, self.min_velocity, self.max_velocity)
+        if self.emergency_braking:
+            return 0.0  # stop the car
+        else:
+            return self.velocity 
 
-        # if angle > 20:
-            # return 4.0
-        # elif angle > 10:
-            # return 5.0
-        # else:
-            # return 6.0
+    def calculate_acceleration(self, final_velocity, initial_velocity, time):
+        if self.emergency_braking:
+            return (final_velocity - initial_velocity) / time # deceleration formula
+        else:
+            return 1.0  # standard acceleration
+        
+    def value_mapper(self, value, istart, istop, ostart, ostop):
+        value = ostart + (ostop - ostart) * \
+            ((value - istart) / (istop - istart))
+        return round(value, 1)
 
     def publish_steering(self, speed, angle):
         msg = AckermannDriveStamped()
-        msg.drive.speed = 1.0
+        msg.drive.speed = speed
         msg.drive.acceleration = 1.0
-        msg.drive.jerk = 1.0
         msg.drive.steering_angle = angle
-        msg.drive.steering_angle_velocity = 1.0
-        self.steering_pub.publish(msg)
+        msg.drive.jerk = 1.0
+        self.drive_pub.publish(msg)
 
 
 def main(args=None):
